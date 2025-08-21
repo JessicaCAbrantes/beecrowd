6ed6ec9f31636d417bac7fe5ff088054a3751a52
Jéssica Abrantes <152931139+JessicaCAbrantes@users.noreply.github.com>
2025-08-21 09:06:17 -0300

    Create 1001.py

---
+++ solution/1001.py
@@ -0,0 +1,23 @@
+'''
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X.
+Print X as shown below. Print endline after the result otherwise you will get “_Presentation Error_”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, 
+according to the following example.
+
+Obs.: don't forget the endline after all.
+'''
+
+#Leitura dos valores
+A = int(input())
+B = int(input())
+
+#Soma
+X = A + B
+
+#Saída no formato exigido
+print(f"X = {X}")
