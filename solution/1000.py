be21f025bce35d7d8625ffc4fd2d16610323967f
Jéssica Abrantes <152931139+JessicaCAbrantes@users.noreply.github.com>
2025-08-21 08:59:24 -0300

    Create 1000.py
    first solution

---
+++ solution/1000.py
@@ -0,0 +1,7 @@
+#Problem:  - Hello World!
+#Language: Python 3.11 (Python 3.11.4)
+
+#You must print the message Hello World! and then the endline as shown below.
+
+#Solução em Pyhton 3.11:
+print("Hello World!")
